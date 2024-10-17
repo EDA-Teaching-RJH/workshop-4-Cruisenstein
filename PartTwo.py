477186790674c14a89eb89ae2167d62ead606271
Cruisenstein <Cruisenstein00@gmail.com>
2024-10-17 16:19:20 +0100

    more

--- PartTwo.py
+++ PartTwo.py
@@ -1,3 +1,4 @@
+
 wallet = 0
 
 while wallet < 75:
@@ -5,7 +6,7 @@ while wallet < 75:
     if wallet >= 75:
         break
     else:
-        print("------A coffee costs 75p.------")
+        print("------PLEASE ENTER 75p TO BEGIN.------")
         print(f"You have {wallet}p in credit")
         more = int(input("Please enter a coin: "))
 
@@ -17,9 +18,35 @@ while wallet < 75:
                 print("Error invaild input")
                 print("this machine only accepts 50p, 20p, 10p & 5p coins.")
 
-wallet = wallet - 75
-print("------Thank you, enjoy your coffee.------")
-print(f"           Your change is {wallet}p")
+print("---------MENU---------")
+print("1: Americano:     70p")
+print("2: Mocha:         75p")
+print("3: Latte:         70p")
+print("4: Iced Latte:    75p")
+print("5: Hot Cholocate: 55p")
+pick = int(input("Please make a selection using the numbers: "))
+
+match pick:
+    case 1:
+        print("------Thank you, enjoy your Americano.------")
+        purhase = 70
+    case 2:
+        print("------Thank you, enjoy your Mocha.------")
+        purhase = 75
+    case 3:
+        print("------Thank you, enjoy your Latte------")
+        purhase = 70
+    case 4:
+        print("------Thank you, enjoy your Iced Latte.------")
+        purhase = 75
+    case 5:
+        print("------Thank you, enjoy your Hot Chocolate.------")
+        purhase = 55
+    case _:
+        print("That's not on the menu!")
+
+wallet = wallet - purhase
+print(f"Your change is {wallet}p")
 
 
 
