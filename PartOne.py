4942258d4fe0998c7eeb735fa099e83274e142de
Cruisenstein <Cruisenstein00@gmail.com>
2024-10-17 14:27:00 +0100

    done

--- PartOne.py
+++ PartOne.py
@@ -1,1 +1,11 @@
+var = str(input("What would you like rewrote in snake case?"))
+list(var)
+
+for i in var:
+    if i.isupper():
+        var += "_" + i.lower()
+    else:
+        var += i
+
+print(var)
 
